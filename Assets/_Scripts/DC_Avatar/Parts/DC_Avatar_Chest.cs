d7a1a098a050b527a371c9a5ddf83d76a52293b1
Ben Otter <benotter@me.com>
2017-08-23 04:41:13 -0700

    Began initial UI stuff, continued damage engine

--- Assets/_Scripts/DC_Avatar/Parts/DC_Avatar_Chest.cs
+++ Assets/_Scripts/DC_Avatar/Parts/DC_Avatar_Chest.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DC_Avatar_Chest : MonoBehaviour 
+public class DC_Avatar_Chest : DC_Avatar_Part 
 {
 	public Color playerColor = Color.blue;
 	public Color damageColor = Color.red * new Color(1f,1f,1f,0.5f);
+
+	public bool broken = false;
 
 
 	private MeshRenderer rend;
---
+++ Assets/_Scripts/DC_Avatar/Parts/DC_Avatar_Part.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DC_Avatar_Part : MonoBehaviour 
+{
+
+	
+}
